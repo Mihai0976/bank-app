e648d6bd13257da9bb2a5def5920259a4f6d7a57
Mihai0976 <mihai.sorinescu@ha.ax>
2023-04-20 23:15:03 +0300

    Styled login form and added button  to create new acount  user

--- bank-app/src/pages/Login.js
+++ bank-app/src/pages/Login.js
@@ -2,16 +2,20 @@
 const Login = () => {
  return ( 
   
-  
-   <div className="login">
-     <p className="title">LogIn</p>
+  <div>
+     <div className="login">
+      
+      <div className="title-bank-login"><h2>Consens</h2><h2 className="h1Bank">Bank</h2></div> 
+       
+     <p className="login-title"> Log In</p>
       <form> 
-        <input type="email" /><p />
-        <input type="password" /><p />
-        <input type={"submit"} 
-          style={{ backgroundColor:   "#a1eafb" }} />
-      </form>
+        <input className="input-login" type="email" placeholder="User ID"/><p />
+        <input className="input-login" type="password" placeholder="Password"/><p />
+        <input className="login-btn" type={"submit"} value={"Login"}/>
+     </form>
      </div>
+     <button className="new-client-btn">New to us? <a href="">Create new acount</a> </button>
+   </div>
   );
 }
  
